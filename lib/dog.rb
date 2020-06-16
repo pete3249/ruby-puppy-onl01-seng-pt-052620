70b17462d899db7293d0cc4336a105b2a6a38a97
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-16 14:17:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+require 'pry'
+
+class Dog
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    @@all.each do |dog|
+      
+    end 
+  end 
+    
+  
+  
+  
+  
+end 
